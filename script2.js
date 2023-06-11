5ab85d4b99985e14d0dfb80416857f221ec15b27
Otniel Edward <otnieledwardd@gmail.com:qclearcleargit config --global user.email otnieledwardd@gmail.com>
2023-06-11 17:50:00 +0700

    pomodoro timer 25+5 FCC challenge

---
+++ script2.js
@@ -0,0 +1,105 @@
+const breakDecrementButton = document.getElementById("break-decrement");
+const breakIncrementButton = document.getElementById("break-increment");
+const sessionDecrementButton = document.getElementById("session-decrement");
+const sessionIncrementButton = document.getElementById("session-increment");
+const breakLengthElement = document.getElementById("break-length");
+const sessionLengthElement = document.getElementById("session-length");
+const timerLabelElement = document.getElementById("timer-label");
+const timeLeftElement = document.getElementById("time-left");
+const startStopButton = document.getElementById("start_stop");
+const resetButton = document.getElementById("reset");
+const beepSound = document.getElementById("beep");
+
+let breakLength = 5;
+let sessionLength = 25;
+let isSession = true;
+let isRunning = false;
+let timeRemaining;
+let timerInterval;
+
+function updateBreakLength(value) {
+  if (!isRunning) {
+    breakLength += value;
+    if (breakLength < 1) {
+      breakLength = 1;
+    } else if (breakLength > 60) {
+      breakLength = 60;
+    }
+    breakLengthElement.textContent = breakLength;
+  }
+}
+
+function updateSessionLength(value) {
+  if (!isRunning) {
+    sessionLength += value;
+    if (sessionLength < 1) {
+      sessionLength = 1;
+    } else if (sessionLength > 60) {
+      sessionLength = 60;
+    }
+    sessionLengthElement.textContent = sessionLength;
+    timeRemaining = sessionLength * 60;
+    updateTimerDisplay();
+  }
+}
+
+function updateTimerDisplay() {
+  const minutes = Math.floor(timeRemaining / 60);
+  const seconds = timeRemaining % 60;
+  const displayTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  timeLeftElement.textContent = displayTime;
+}
+
+function toggleTimer() {
+  isRunning = !isRunning;
+  if (isRunning) {
+    startStopButton.textContent = "Stop";
+    timerInterval = setInterval(decrementTimer, 1000);
+  } else {
+    startStopButton.textContent = "Start";
+    clearInterval(timerInterval);
+  }
+}
+
+function decrementTimer() {
+  if (timeRemaining > 0) {
+    timeRemaining--;
+    updateTimerDisplay();
+  } else {
+    beepSound.play();
+    if (isSession) {
+      isSession = false;
+      timerLabelElement.textContent = "Break";
+      timeRemaining = breakLength * 60;
+      updateTimerDisplay();
+    } else {
+      isSession = true;
+      timerLabelElement.textContent = "Session";
+      timeRemaining = sessionLength * 60;
+      updateTimerDisplay();
+    }
+  }
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  breakLength = 5;
+  sessionLength = 25;
+  isSession = true;
+  isRunning = false;
+  timeRemaining = sessionLength * 60;
+  breakLengthElement.textContent = breakLength;
+  sessionLengthElement.textContent = sessionLength;
+  timerLabelElement.textContent = "Session";
+  updateTimerDisplay();
+  startStopButton.textContent = "Start";
+  beepSound.pause();
+  beepSound.currentTime = 0;
+}
+
+breakDecrementButton.addEventListener("click", () => updateBreakLength(-1));
+breakIncrementButton.addEventListener("click", () => updateBreakLength(1));
+sessionDecrementButton.addEventListener("click", () => updateSessionLength(-1));
+sessionIncrementButton.addEventListener("click", () => updateSessionLength(1));
+startStopButton.addEventListener("click", toggleTimer);
+resetButton.addEventListener("click", resetTimer);
